873ac436ae15e53631756eacd145a1dfd8bddced
mzaini30 <muhzaini30@gmail.com>
2022-09-01 15:36:01 +0800

    upload

--- new.mjs
+++ new.mjs
@@ -1,6 +1,37 @@
 import { question } from "readline-sync";
 import { slug } from "kumpulan-tools";
+import { writeFileSync } from "fs";
 
 const judul = question("Judul: ");
-// console.log(judul);
-// console.log(slug(judul));
+
+const tanggalan = new Date();
+const tanggal = tanggalan.getDate();
+const bulan = tanggalan.getMonth();
+const tahun = tanggalan.getFullYear();
+
+const list_bulan = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const sekarang = `${tanggal} ${list_bulan[bulan]} ${tahun}`;
+
+const isi = `---
+layout: "@/layouts/BlogPost"
+title: "${judul}"
+publishDate: ${sekarang}
+description: ""
+tags: [""]
+---
+`;
+
+writeFileSync(`src/pages/posts/${slug(judul)}.mdx`, isi);
